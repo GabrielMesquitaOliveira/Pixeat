4a90fa5895cd4164cbd09fb9bf183829408ba1f1
Gabriel <92613742+GabrielMesquitaOliveira@users.noreply.github.com>
2025-11-09 15:14:20 -0300

    chore: sidebar and shadcn

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as React from "react"
+import Image from "next/image"
 import {
   IconCamera,
   IconChartBar,
@@ -161,8 +162,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
               <a href="#">
-                <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+                <Image src="/logo.svg" alt="Logo" width={128} height={128} />
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
@@ -170,7 +170,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavDocuments items={data.documents} />
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
